d99937cbe2b04116911463f9fc07a006e7cf8204
Tomas <tomas.gonzalez@hotmail.com>
2025-01-21 19:08:09 -0300

    Initial Commit
    Proposed architecture for the project, without initializing peripherals, the FSM is just a toggle.

---
+++ Core/Src/fsm_mode.c
@@ -0,0 +1,70 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : mef_mode.c
+  * @brief          : Main Mef file
+  ******************************************************************************
+  *
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include <fsm_mode.h>
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+void fsm_mode(void)
+{
+
+
+  while (1)
+  {
+  //TODO MEF Principal, en la kl46 usabamos systick para los timers, pensar que hacemos aca.
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_SET);//LED ON
+  HAL_Delay(5000);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);//LED OFF
+  HAL_Delay(5000);
+  }
+}
+
+
