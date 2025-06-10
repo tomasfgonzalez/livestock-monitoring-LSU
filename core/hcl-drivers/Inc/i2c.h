584a8afe09f9b65cfdba95c69b35c33df1054e5a
Brimix <only.brimix@gmail.com>
2025-06-09 23:32:06 -0300

    Testing

---
+++ core/hcl-drivers/Inc/i2c.h
@@ -0,0 +1,42 @@
+/**
+  ******************************************************************************
+  * @authors        : Tomas Gonzalez & Brian Morris
+  * @file           : i2c.h
+  * @brief          : Header for I2C file
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 Gonazalez & Morris.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __I2C_H__
+#define __I2C_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <stdbool.h>
+
+/* Structure declarations ----------------------------------------------------*/
+extern I2C_HandleTypeDef hi2c1;
+
+/* Public functions ----------------------------------------------------------*/
+void I2C_Init(void);
+bool I2C_hasError(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __I2C_H__ */
+
